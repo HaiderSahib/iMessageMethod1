60460255c0c6d9efb8eaed10a23c149fca094dc2
Haider <haidersahib@gmail.com>
2016-06-20 16:13:37 -0400

    First commit

---
+++ platforms/ios/MessagesExtension/MessagesViewController.h
@@ -0,0 +1,13 @@
+//
+//  MessagesViewController.h
+//  MessagesExtension
+//
+//  Created by Haider Sahib on 6/16/16.
+//
+//
+
+#import <Messages/Messages.h>
+#import "Cordova/CDVViewController.h"
+@interface MessagesViewController : MSMessagesAppViewController
+
+@end
